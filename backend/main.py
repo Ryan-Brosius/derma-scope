e8da4ddce638bce6c40a558d9c8a61b8f23a2b0e
Ryan Brosius <ryanbrosius00@gmail.com>
2025-03-01 16:48:30 -0500

    Fixing tokens lol

--- backend/main.py
+++ backend/main.py
@@ -37,7 +37,7 @@ async def generate_information_report(
 
     image_prediction = classify_image(image)
 
-    return ReportGenerator.generate_report(age, gender, history, symptoms, image_prediction, lesion_location, max_tokens=10)
+    return ReportGenerator.generate_report(age, gender, history, symptoms, image_prediction, lesion_location)
 
 if __name__ == "__main__":
     import uvicorn
