388d952da594c7ff5e6a6f9e14606b6419aac54e
Ryan Brosius <ryanbrosius00@gmail.com>
2025-03-01 14:58:07 -0500

    Adding api call to backend

--- frontend/api_caller.js
+++ frontend/api_caller.js
@@ -1,23 +1,36 @@
 
 
-  fetch('http://localhost:8000/health/', { // not sure if this should be the docs or the health url
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'SHA256:gJAz8nq64XDgCicEqbx1ohBLHGGO6o28ehdKsUXc3jo' // api key? idk if it even needs one
-    },
-    credentials: 'include'
-  })
-  .then(response => {
+
+async function sendDiagnosisData(image, age, gender, history, symptoms, lesion_location) {
+
+  const formData = new FormData();
+
+  formData.append("image", image);
+  formData.append("age", age);
+  formData.append("gender", gender);
+  formData.append("history", history);
+  formData.append("symptoms", symptoms);
+  formData.append("lesion_location", lesion_location);
+  
+  try {
+    const response = await fetch("http://localhost:8000/diagnose", {
+        method: "POST",
+        body: formData,
+    });
+
+    console.log(response)
+
     if (!response.ok) {
-      throw new Error('Network response error');
+        throw new Error("Failed to send the request.");
     }
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Fetch error:', error);
-  });
+
+    const data = await response.json();
+    console.log("Diagnosis Report:", data);
+    alert("Report generated successfully!");
+  } catch (error) {
+      console.error("Error:", error);
+      alert("An error occurred while generating the report.");
+  }
+}
+
 
